e33e10bc53d41bba4dad5bfa4fad961fede2ad3e
Matheuzao-lab <matheus.orlovski@escola.pr.gov.br>
2025-06-12 08:03:44 -0300

    fgjrmn

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Times BR',
+    'Qual time brasileiro tem mais brasileirão?',
+    'Palmeiras'
+)
+
+criaCartao(
+    'Times BR',
+    'Qual time brasileiro tem mais mundiais?',
+    'São Paulo'
+)
+
+criaCartao(
+    'Times do mundo',
+    'Qual time tem mais libertadores?',
+    'Club Atlético Independiente'
+)
+
+criaCartao(
+    'Times do mundo',
+    'Qual time tem mais Champions?',
+    'Real Madrid'
+)
